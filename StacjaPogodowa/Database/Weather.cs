1bf4f695bbf7ee0ab28fa9a858771ada9209a099
uczen <uczen@STANISLAW>
2025-10-03 15:40:27 +0200

    added

---
+++ StacjaPogodowa/Database/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string MeasuredBy { get; set; } = string.Empty;
+        public DateTime MeasuredByDate { get; set; }
+    }
+}
