cbfe1761842dcf527dee7e6306f4c34ef50b4f34
uczen <uczen@STANISLAW>
2025-10-17 15:45:09 +0200

    ````

---
+++ StacjaPogodowa/StacjaPogodowa/Utils.cs
@@ -0,0 +1,23 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacjaPogodowa
+{
+    internal class Utils
+    {
+        internal static Weather ChangeWeather(Weather weather)
+        {
+            var objectToChange = new Weather();
+            Console.WriteLine($"Weather measured by: {weather.MeasuredBy}");
+            weather.MeasuredBy = "Jan Kowalski";
+            Console.WriteLine($"Weather measured by:{weather.MeasuredBy}");
+            return objectToChange;
+        }
+        
+
+    }
+}
