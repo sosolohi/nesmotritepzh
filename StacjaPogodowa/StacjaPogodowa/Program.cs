3ae25802c1aeaab28bef965d2481244adda1964b
uczen <uczen@STANISLAW>
2025-10-17 15:28:36 +0200

    olowek

--- StacjaPogodowa/StacjaPogodowa/Program.cs
+++ StacjaPogodowa/StacjaPogodowa/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Database;
+
+Console.WriteLine("Hello, World!"); 
 Console.ReadKey();
+Weather weather1 = new Weather();
+Weather weather5= new Weather();
+weather5= ChangeWeather(weather1);
+
+
+Weather ChangeWeather(Weather weather)
+{
+    var objectToChange = new Weather();
+    Console.WriteLine($"Weather measured by: {weather.MeasuredBy}");
+    weather.MeasuredBy = "Jan Kowalski";
+    Console.WriteLine($"Weather measured by:{weather.MeasuredBy}");
+    return objectToChange;
+}
+
